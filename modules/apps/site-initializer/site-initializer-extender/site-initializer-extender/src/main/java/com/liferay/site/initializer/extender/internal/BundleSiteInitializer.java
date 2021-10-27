7fd5fbaef4b28f0b9a75573f0aa06f98e9509529
Victor Trajano <victor.trajano@liferay.com>
2021-10-27 09:23:18 -0300

    LPS-141387 Get javascript by file

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -2094,6 +2094,12 @@ public class BundleSiteInitializer implements SiteInitializer {
 			return;
 		}
 
+		String js = _read(resourcePath + "/js.js");
+
+		if (Validator.isNotNull(js)) {
+			settingsJSONObject.put("javascript", js);
+		}
+
 		UnicodeProperties unicodeProperties = layoutSet.getSettingsProperties();
 
 		String js = GetterUtil.getString(_read(resourcePath + "/js.js"));
