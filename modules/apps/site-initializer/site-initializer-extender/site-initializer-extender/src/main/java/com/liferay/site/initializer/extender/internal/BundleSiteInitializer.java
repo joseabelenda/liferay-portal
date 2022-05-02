080941bb07ab9e3b1b87eeb7f8f54fff234ee241
Nilton Vieira <nilton.vieira@liferay.com>
2022-05-02 11:30:24 -0300

    LPS-152469 avoid NPE

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -3368,6 +3368,10 @@ public class BundleSiteInitializer implements SiteInitializer {
 
 			Organization organization = organizationsPage.fetchFirstItem();
 
+			if(organization == null){
+				continue;
+			}
+
 			_userLocalService.addOrganizationUser(
 				GetterUtil.getLong(organization.getId()), userId);
 		}
