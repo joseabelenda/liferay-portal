1db9fbe74a1718edfe7c642a8f96280b42edeeee
Mylena Monte <mylena.monte@liferay.com>
2022-11-10 22:00:48 -0300

    Document Contributor Example | Create Service

---
+++ modules/apps/document-contributor/document-contributor-service/src/main/resources/META-INF/sql/tables.sql
@@ -0,0 +1,12 @@
+create table Documents_Documents (
+	documentId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	name VARCHAR(75) null,
+	description VARCHAR(75) null,
+	link VARCHAR(75) null
+);
