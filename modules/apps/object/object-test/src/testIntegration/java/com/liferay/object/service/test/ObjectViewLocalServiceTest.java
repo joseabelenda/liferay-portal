f36360ad6d89454bc94ea1e10ee809f52c2c2fe8
José Abelenda <jose.abelenda@liferay.com>
2023-07-11 17:32:34 -0300

    LPS-188425 Assert ERC on testUpdateObjectView

--- modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectViewLocalServiceTest.java
+++ modules/apps/object/object-test/src/testIntegration/java/com/liferay/object/service/test/ObjectViewLocalServiceTest.java
@@ -262,8 +262,10 @@ public class ObjectViewLocalServiceTest {
 	public void testUpdateObjectView() throws Exception {
 		ObjectView objectView = _addObjectView();
 
+		String externalReferenceCode = RandomTestUtil.randomString();
+
 		objectView = _objectViewLocalService.updateObjectView(
-			objectView.getExternalReferenceCode(), objectView.getObjectViewId(),
+			externalReferenceCode, objectView.getObjectViewId(),
 			objectView.isDefaultObjectView(), objectView.getNameMap(),
 			Collections.singletonList(_createObjectViewColumn("Fox", "fox")),
 			Collections.emptyList(),
@@ -281,6 +283,9 @@ public class ObjectViewLocalServiceTest {
 
 		Assert.assertEquals(
 			objectViewSortColumns.toString(), 1, objectViewSortColumns.size());
+
+		Assert.assertEquals(
+			externalReferenceCode, objectView.getExternalReferenceCode());
 
 		_assertFailureAddOrUpdateObjectView(
 			ObjectViewColumnFieldNameException.class,
