450424c7731f101f378039ff9af951936c479986
José Abelenda <jose.abelenda@liferay.com>
2023-09-12 10:15:09 -0300

    LPS-188425 Auto SF

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/DuplicateObjectViewExternalReferenceCodeException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/DuplicateObjectViewExternalReferenceCodeException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.object.exception;
 
 import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeException;
@@ -9,7 +10,8 @@ import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeExcepti
 /**
  * @author Marco Leo
  */
-public class DuplicateObjectViewExternalReferenceCodeException extends DuplicateExternalReferenceCodeException {
+public class DuplicateObjectViewExternalReferenceCodeException
+	extends DuplicateExternalReferenceCodeException {
 
 	public DuplicateObjectViewExternalReferenceCodeException() {
 	}
@@ -18,11 +20,15 @@ public class DuplicateObjectViewExternalReferenceCodeException extends Duplicate
 		super(msg);
 	}
 
-	public DuplicateObjectViewExternalReferenceCodeException(String msg, Throwable throwable) {
+	public DuplicateObjectViewExternalReferenceCodeException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
-	public DuplicateObjectViewExternalReferenceCodeException(Throwable throwable) {
+	public DuplicateObjectViewExternalReferenceCodeException(
+		Throwable throwable) {
+
 		super(throwable);
 	}
 
