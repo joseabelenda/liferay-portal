77e545b0e3f4be51c34e48f909f685daf9bf1866
José Abelenda <jose.abelenda@liferay.com>
2023-11-07 14:07:59 -0300

    LUG - CX

--- workspaces/liferay-lug-workspace/client-extensions/liferay-lug-etc-spring-boot/src/main/java/com/liferay/sample/ObjectAction1RestController.java
+++ workspaces/liferay-lug-workspace/client-extensions/liferay-lug-etc-spring-boot/src/main/java/com/liferay/sample/ObjectAction1RestController.java
@@ -1,17 +1,16 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
 package com.liferay.sample;
 
-import com.liferay.petra.string.StringBundler;
+import java.net.URI;
+
+import java.util.function.Function;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import java.net.URL;
-import java.net.URLConnection;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,7 +25,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriBuilder;
 
 /**
  * @author José Abelenda
@@ -37,24 +38,27 @@ public class ObjectAction1RestController extends BaseRestController {
 
 	@PostMapping
 	public ResponseEntity<String> post(
-			@AuthenticationPrincipal Jwt jwt, @RequestBody String json) {
-
-		// System.out.println("post");
+		@AuthenticationPrincipal Jwt jwt, @RequestBody String json) {
 
 		JSONObject jsonObject = new JSONObject(json);
 
-		JSONObject commerceOrderJSONObject = jsonObject.getJSONObject("commerceOrder");
+		long commerceOrderId = jsonObject.getLong("commerceOrderId");
 
-		System.out.println("commerceOrderJSONObject: " + commerceOrderJSONObject);
+		JSONObject commerceOrderItemsJSONObject = _getCommerceOrderItems(
+			commerceOrderId, jwt);
 
-		long accountId = commerceOrderJSONObject.getLong("accountId");
+		JSONArray orderItemsJSONArray =
+			commerceOrderItemsJSONObject.getJSONArray("items");
 
-		System.out.println("accountId: " + accountId);
+		JSONObject commerceOrderJSONObject = jsonObject.getJSONObject(
+			"commerceOrder");
 
-		JSONArray orderItemsJSONArray = commerceOrderJSONObject.getJSONArray("orderItems");
+		String creatorEmailAddress = commerceOrderJSONObject.getString(
+			"creatorEmailAddress");
 
 		if (orderItemsJSONArray != null) {
-			JSONObject orderItemJSONObject = orderItemsJSONArray.getJSONObject(0);
+			JSONObject orderItemJSONObject = orderItemsJSONArray.getJSONObject(
+				0);
 
 			if (orderItemJSONObject != null) {
 				String sku = orderItemJSONObject.getString("sku");
@@ -62,37 +66,111 @@ public class ObjectAction1RestController extends BaseRestController {
 				if (sku != null) {
 					System.out.println("\n\n\t\t*****SKU: " + sku);
 
-					WebClient.Builder builder = WebClient.builder();
-
-					WebClient webClient = builder.baseUrl(
-							lxcDXPServerProtocol + "://" + lxcDXPMainDomain).defaultHeader(
-									HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
-							.defaultHeader(
-									HttpHeaders.AUTHORIZATION, "Bearer " + jwt.getTokenValue())
-							.defaultHeader(
-									HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-							.build();
-
 					long organizationId = 0;
 
 					if (sku.equals("BASIC")) {
-						organizationId = 33420;
-					} else if (sku.equals("PREMIUM")) {
-						organizationId = 33414;
-
+						organizationId = _getOrganizationId("BASIC", jwt);
+					}
+					else if (sku.equals("PREMIUM")) {
+						organizationId = _getOrganizationId("PREMIUM", jwt);
 					}
 
-					webClient.post().uri(
-							"o/headless-admin-user/v1.0/accounts/" + accountId + "/organizations/" + organizationId);
+					long finalOrganizationId = organizationId;
+
+					_post(
+						null, jwt,
+						uriBuilder -> uriBuilder.path(
+							"o/headless-admin-user/v1.0/organizations/" +
+								finalOrganizationId +
+									"/user-accounts/by-email-address/" +
+										creatorEmailAddress
+						).build());
 				}
 			}
-
 		}
 
 		return new ResponseEntity<>(json, HttpStatus.OK);
 	}
 
+	private JSONObject _get(Function<UriBuilder, URI> uriFunction, Jwt jwt) {
+		return new JSONObject(
+			_getWebClient(
+			).get(
+			).uri(
+				uriBuilder -> uriFunction.apply(uriBuilder)
+			).accept(
+				MediaType.APPLICATION_JSON
+			).header(
+				HttpHeaders.AUTHORIZATION, "Bearer " + jwt.getTokenValue()
+			).retrieve(
+			).bodyToMono(
+				String.class
+			).block());
+	}
+
+	private JSONObject _getCommerceOrderItems(long commerceOrderId, Jwt jwt) {
+		return _get(
+			uriBuilder -> uriBuilder.path(
+				"/o/headless-commerce-admin-order/v1.0/orders/" +
+					commerceOrderId + "/orderItems"
+			).queryParam(
+				"page", "1"
+			).queryParam(
+				"pageSize", "-1"
+			).build(),
+			jwt);
+	}
+
+	private long _getOrganizationId(String externalReferenceCode, Jwt jwt) {
+		JSONObject jsonObject = _get(
+			uriBuilder -> uriBuilder.path(
+				"/o/headless-admin-user/v1.0/organizations/by-external-reference-code/" +
+					externalReferenceCode
+			).queryParam(
+				"page", "1"
+			).queryParam(
+				"pageSize", "-1"
+			).build(),
+			jwt);
+
+		return jsonObject.getLong("id");
+	}
+
+	private WebClient _getWebClient() {
+		return WebClient.builder(
+		).baseUrl(
+			lxcDXPServerProtocol + "://" + lxcDXPMainDomain
+		).exchangeStrategies(
+			ExchangeStrategies.builder(
+			).codecs(
+				clientCodecConfigurer -> clientCodecConfigurer.defaultCodecs(
+				).maxInMemorySize(
+					5 * 1024 * 1024
+				)
+			).build()
+		).build();
+	}
+
+	private void _post(
+		Object bodyValue, Jwt jwt, Function<UriBuilder, URI> uriFunction) {
+
+		_getWebClient(
+		).post(
+		).uri(
+			uriBuilder -> uriFunction.apply(uriBuilder)
+		).accept(
+			MediaType.APPLICATION_JSON
+		).contentType(
+			MediaType.APPLICATION_JSON
+		).header(
+			HttpHeaders.AUTHORIZATION, "Bearer " + jwt.getTokenValue()
+		).retrieve(
+		).bodyToMono(
+			Void.class
+		).block();
+	}
+
 	private static final Log _log = LogFactory.getLog(
-			ObjectAction1RestController.class);
+		ObjectAction1RestController.class);
 
 }
