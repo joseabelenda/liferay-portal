05a5ed59c214c33d03b1d5bf91d52d895857d2e9
Nilton Vieira <nilton.vieira@liferay.com>
2025-10-31 12:02:58 -0300

    LRSD-10733 RAGService

---
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/configuration/VertexAiEmbeddingConfiguration.java
@@ -0,0 +1,50 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.learn.configuration;
+
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.aiplatform.v1.PredictionServiceSettings;
+
+import java.io.ByteArrayInputStream;
+
+import java.util.List;
+
+import org.springframework.ai.vertexai.embedding.VertexAiEmbeddingConnectionDetails;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Nilton Vieira
+ */
+@Configuration
+public class VertexAiEmbeddingConfiguration {
+
+	@Bean
+	public VertexAiEmbeddingConnectionDetails
+			vertexAiEmbeddingConnectionDetails()
+		throws Exception {
+
+		return new VertexAiEmbeddingConnectionDetails(
+			"devcon25-476012", "us-central1", "google",
+			PredictionServiceSettings.newBuilder(
+			).setCredentialsProvider(
+				FixedCredentialsProvider.create(
+					GoogleCredentials.fromStream(
+						new ByteArrayInputStream(
+							_googleRAGCredentials.getBytes())
+					).createScoped(
+						List.of(
+							"https://www.googleapis.com/auth/cloud-platform")
+					))
+			).build());
+	}
+
+	@Value("${liferay.learn.google.rag.credentials}")
+	private String _googleRAGCredentials;
+
+}
---
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-spring-boot/src/main/java/com/liferay/learn/service/RAGService.java
@@ -0,0 +1,88 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.learn.service;
+
+import com.liferay.client.extension.util.spring.boot3.service.BaseService;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+
+import java.util.List;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.qdrant.QdrantVectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Nilton Vieira
+ */
+@Service
+public class RAGService extends BaseService {
+
+	public void addOrUpdateDocument(
+		long assetEntryId, String assetEntryType, String content) {
+
+		Document document = new Document(
+			content,
+			HashMapBuilder.<String, Object>put(
+				"assetEntryId", assetEntryId
+			).put(
+				"assetEntryType", assetEntryType
+			).build());
+
+		List<Document> splittedDocuments = new TokenTextSplitter(
+		).split(
+			document
+		);
+
+		_vectorStore.doAdd(splittedDocuments);
+	}
+
+	public String search(String question) {
+		List<Document> vectorStoreResult = _vectorStore.doSimilaritySearch(
+			SearchRequest.builder(
+			).query(
+				question
+			).topK(
+				5
+			).similarityThreshold(
+				0.6
+			).build());
+
+		StringBundler sb = new StringBundler();
+
+		for (Document document : vectorStoreResult) {
+			sb.append(
+				document.getText()
+			).append(
+				System.lineSeparator()
+			);
+		}
+
+		return _chatClient.prompt(
+		).user(
+			StringBundler.concat(
+				"You are the liferay learn assistant to provide accurate ",
+				"answers to the question in the QUESTION section. If unsure, ",
+				"simply state that you do not know. DOCUMENTS: ", sb,
+				"QUESTION: ", question)
+		).call(
+		).content();
+	}
+
+	private ChatClient _chatClient;
+
+	@Autowired
+	private EmbeddingModel _embeddingModel;
+
+	@Autowired
+	private QdrantVectorStore _vectorStore;
+
+}
