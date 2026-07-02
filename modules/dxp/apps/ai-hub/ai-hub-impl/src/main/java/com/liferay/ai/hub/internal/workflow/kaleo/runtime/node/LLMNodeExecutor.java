32f852697a294c5f48c5161e558d39234f4c5c27
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-07-01 22:34:54 -0300

    LPD-96617 feat: when there is no content to be injected to the LLM throws an exception to avoid inference and non grounded responses

--- modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/kaleo/runtime/node/LLMNodeExecutor.java
+++ modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/kaleo/runtime/node/LLMNodeExecutor.java
@@ -8,6 +8,7 @@ package com.liferay.ai.hub.internal.workflow.kaleo.runtime.node;
 import com.liferay.ai.hub.guardrail.ModelArmorHandler;
 import com.liferay.ai.hub.internal.assistant.handler.AssistantHandlerContext;
 import com.liferay.ai.hub.internal.assistant.handler.AssistantHandlerUtil;
+import com.liferay.ai.hub.internal.langchain4j.observability.api.listener.AiServiceErrorListenerImpl;
 import com.liferay.ai.hub.internal.langchain4j.observability.api.listener.InputGuardrailExecutedListenerImpl;
 import com.liferay.ai.hub.internal.langchain4j.observability.api.listener.OutputGuardrailExecutedListenerImpl;
 import com.liferay.ai.hub.internal.mcp.tool.provider.MCPToolProviderUtil;
@@ -15,6 +16,7 @@ import com.liferay.ai.hub.internal.model.VertexAiGeminiUtil;
 import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.GuardrailsUtil;
 import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.KaleoNodeSettingUtil;
 import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.MessageUtil;
+import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.OnErrorConsumerUtil;
 import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.PromptUtil;
 import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.QuotaUtil;
 import com.liferay.ai.hub.internal.workflow.kaleo.runtime.node.util.RetrievalAugmentorUtil;
@@ -61,6 +63,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Consumer;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -155,10 +158,15 @@ public class LLMNodeExecutor extends BaseNodeExecutor {
 			_objectEntryManager, outputGuardrails, _quotaManager,
 			serviceContext, workflowContext);
 
+		Consumer<Throwable> onErrorConsumer = OnErrorConsumerUtil.create(
+			kaleoInstanceToken, sseEventSinkKey,
+			vertexAiGeminiStreamingChatModel);
+
 		AssistantHandlerUtil.handle(
 			AssistantHandlerContext.builder(
 			).aiServiceListeners(
 				List.of(
+					new AiServiceErrorListenerImpl(onErrorConsumer),
 					new InputGuardrailExecutedListenerImpl(executionContext),
 					new OutputGuardrailExecutedListenerImpl(executionContext))
 			).inputGuardrails(
@@ -185,13 +193,7 @@ public class LLMNodeExecutor extends BaseNodeExecutor {
 					}
 				}
 			).onErrorConsumer(
-				throwable -> {
-					MCPToolProviderUtil.close(sseEventSinkKey);
-
-					vertexAiGeminiStreamingChatModel.close();
-
-					_log.error(throwable);
-				}
+				onErrorConsumer
 			).outputGuardrails(
 				outputGuardrails
 			).retrievalAugmentor(
@@ -287,9 +289,6 @@ public class LLMNodeExecutor extends BaseNodeExecutor {
 			throw new RuntimeException(portalException);
 		}
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		LLMNodeExecutor.class);
 
 	@Reference
 	private DTOConverterRegistry _dtoConverterRegistry;
